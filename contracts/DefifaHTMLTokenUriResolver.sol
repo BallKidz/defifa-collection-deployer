48302d2fb049bf6a3323552ca845a4c564e3d9d4
i001962@gmail.com <i001962@gmail.com>
2023-04-11 19:21:27 -0700

    html resolve w inline js working

--- contracts/DefifaHTMLTokenUriResolver.sol
+++ contracts/DefifaHTMLTokenUriResolver.sol
@@ -47,7 +47,7 @@ contract DefifaHTMLTokenUriResolver is IDefifaTokenUriResolver, IJBTokenUriResol
   address private constant _SCRIPTY_STORAGE_ADDRESS = 0x096451F43800f207FC32B4FF86F286EdaF736eE3;
   address private constant _SCRIPTY_BUILDER_ADDRESS = 0x16b727a2Fc9322C724F4Bc562910c99a5edA5084;
   address private constant _ETHFS_FILESTORAGE_ADDRESS = 0xFc7453dA7bF4d0c739C1c53da57b3636dAb0e11e;
-  uint256 public constant BUFFER_SIZE = 5000000;
+  uint256 public constant BUFFER_SIZE = 1000000;
   //*********************************************************************//
   // --------------------- private stored properties ------------------- //
   //*********************************************************************//
@@ -175,7 +175,7 @@ contract DefifaHTMLTokenUriResolver is IDefifaTokenUriResolver, IJBTokenUriResol
         );
     }  */
      // KMac scripty builder
-      WrappedScriptRequest[] memory requests = new WrappedScriptRequest[](4);
+      WrappedScriptRequest[] memory requests = new WrappedScriptRequest[](3);
 
       requests[0].name = "p5-v1.5.0.min.js.gz";
       requests[0].wrapType = 2; // <script type="text/javascript+gzip" src="data:text/javascript;base64,[script]"></script>
@@ -191,8 +191,8 @@ contract DefifaHTMLTokenUriResolver is IDefifaTokenUriResolver, IJBTokenUriResol
       string memory _team = _tierNameOf[_tier.id];
 
       
-      if (bytes(_title).length < 35) _titleFontSize = '40';
-      else _titleFontSize = '20';
+      if (bytes(_title).length < 35) _titleFontSize = '20';
+      else _titleFontSize = '16';
 
       string memory _fontSize;
       if (bytes(_team).length < 3) _fontSize = '40';
@@ -258,37 +258,37 @@ contract DefifaHTMLTokenUriResolver is IDefifaTokenUriResolver, IJBTokenUriResol
             _title,
            '";let txt_1Size =',
             _titleFontSize,
-           ';let txt_1Color = "#fea282";let txt_1_x = 40;let txt_1_y = 80;',
+           ';let txt_1Color = "#17e4f1";let txt_1_x = 40;let txt_1_y = 80;',
            'let txt_2 ="GAME PHASE ',
             _gamePhase.toString(), ' of 4',
-           '";let txt_2Size = 20;let txt_2Color = "#ed017c";let txt_2_x = 40;let txt_2_y = 120;',
+           '";let txt_2Size = 20;let txt_2Color = "#17e4f1";let txt_2_x = 40;let txt_2_y = 220;',
             'let txt_3 ="',
             _gamePhaseText,
-           '";let txt_3Size = 16;let txt_3Color = "#00ff00";let txt_3_x = 40;let txt_3_y = 160;',
+           '";let txt_3Size = 20;let txt_3Color = "#fea282";let txt_3_x = 40;let txt_3_y = 250;',
             'let txt_4 ="',
              _team,
            '";let txt_4Size =',
             _fontSize,
-            ';let txt_4Color = "#9a8bc7";let txt_4_x = 40;let txt_4_y = 200;',
+            ';let txt_4Color = "#fea282";let txt_4_x = 40;let txt_4_y = 130;',
             'let txt_5 ="TOKEN ID: ',
             _tokenId.toString(),
-           '";let txt_5Size = 20;let txt_5Color = "#c0b3f1";let txt_5_x = 40;let txt_5_y = 240;',
+           '";let txt_5Size = 16;let txt_5Color = "#FFFFFF";let txt_5_x = 40;let txt_5_y = 370;',
             'let txt_6 ="RARITY: ',
             _rarityText,
-           '";let txt_6Size = 20;let txt_6Color = "#c0b3f1";let txt_6_x = 40;let txt_6_y = 280;',
+           '";let txt_6Size = 16;let txt_6Color = "#FFFFFF";let txt_6_x = 40;let txt_6_y = 400;',
             'let font = "data:font/truetype;charset=utf-8;base64,',
             DefifaFontImporter.getSkinnyFontSource(),
-            '";'
+            '";',
            // the p5js js code here
            // TODO remove constant, put on ethfs.xyz, calc buffer size
-           //'let page,camLoc,buttL,buttR,timer,buttonImg,pages=[],numOfPages=2,movingRight=!1,movingLeft=!1,isPaused=!1,artWorkPanel="https://tan-hidden-whippet-249.mypinata.cloud/ipfs/"+artWorkIPFS,scoreCardPanel="https://tan-hidden-whippet-249.mypinata.cloud/ipfs/"+scoreCardIPFS,buttonImage="https://tan-hidden-whippet-249.mypinata.cloud/ipfs/"+buttonImageIPFS,defifaBlue=[19,228,240],txt1=[[txt_1,txt_1Color,txt_1Size]],txt2=[[txt_2,txt_2Color,txt_2Size]],txt3=[[txt_3,txt_3Color,txt_3Size]],txt4=[[txt_4,txt_4Color,txt_4Size]],txt5=[[txt_5,txt_5Color,txt_5Size]],txt6=[[txt_6,txt_6Color,txt_6Size]],pageImg=[];function preload(){pageImg[0]=loadImage(artWorkPanel),pageImg[1]=loadImage(scoreCardPanel),buttonImg=loadImage(buttonImage)}function setup(){myFont=loadFont(font),createCanvas(500,500),camLoc=createVector(0,0);for(let a=0;a<numOfPages;a++)pages[a]=new Page(canvas.width/2*a,0,a,pageImg[a]);timer=canvas.width/2,buttL=new Button(canvas.width/2-90,5,75,75,buttonImg),buttR=new Button(canvas.width/2-90,5,75,75,buttonImg)}function draw(){background(220),slide(),push(),translate(camLoc.x,camLoc.y);for(let a=0;a<numOfPages;a++)pages[a].run();pop(),buttL.run(),buttR.run()}function goRight(){movingLeft||isPaused||(movingRight=!0)}function goLeft(){movingRight||isPaused||(movingLeft=!0)}function slide(){movingRight&&!movingLeft&&0<=timer&&(camLoc.x-=20,timer-=20),movingLeft&&!movingRight&&0<=timer&&(camLoc.x+=20,timer-=20),0==timer&&(movingRight=!1,movingLeft=!1,timer=canvas.width/2),0>=-camLoc.x?(camLoc.x=0,buttL.loc.x=-100):buttL.loc.x=canvas.height/2-75,-camLoc.x>=pages[pages.length-1].loc.x?(timer=canvas.width/2,movingRight=!1,movingLeft=!1,camLoc.x=-pages[pages.length-1].loc.x,buttR.loc.x=-100):buttR.loc.x=canvas.height/2-75}function drawtext(a,b,d){for(var e=a,f=0;f<d.length;++f){var g=d[f],h=g[0],j=g[1],c=g[2],k=textWidth(h);fill(j),textSize(c),text(h,e,b),e+=k}}function mousePressed(){mouseX>buttL.loc.x&&mouseX<buttL.loc.x+buttL.w&&mouseY>buttL.loc.y&&mouseY<buttL.loc.y+buttL.h&&goLeft(),mouseX>buttR.loc.x&&mouseX<buttR.loc.x+buttR.w&&mouseY>buttR.loc.y&&mouseY<buttR.loc.y+buttR.h&&goRight()}class Page{constructor(a,b,c,d){this.loc=createVector(a,b),this.w=canvas.width/2,this.h=canvas.height/2,this.pageNum=c+1,this.img=d}run(){image(this.img,this.loc.x,this.loc.y,500,500),textAlign(LEFT),textFont(myFont),2==this.pageNum&&(drawtext(this.loc.x+txt_1_x,this.loc.y+txt_1_y,txt1),drawtext(this.loc.x+txt_2_x,this.loc.y+txt_2_y,txt2),drawtext(this.loc.x+txt_3_x,this.loc.y+txt_3_y,txt3),drawtext(this.loc.x+txt_4_x,this.loc.y+txt_4_y,txt4),drawtext(this.loc.x+txt_5_x,this.loc.y+txt_5_y,txt5),drawtext(this.loc.x+txt_6_x,this.loc.y+txt_6_y,txt6));3==this.pageNum,line(this.loc.x,this.loc.y,this.loc.x+this.w,this.loc.y),line(this.loc.x,this.loc.y,this.loc.x,this.loc.y+this.h),line(this.loc.x,this.loc.y+this.h,this.loc.x+this.w,this.loc.y+this.h),line(this.loc.x+this.w,this.loc.y,this.loc.x+this.w,this.h)}}class Button{constructor(a,b,c,d,e){this.loc=new createVector(a,b),this.w=c,this.h=d,this.clr="white",this.img=e}run(){this.render(),this.checkMouse()}render(){fill(this.clr),stroke(20),strokeWeight(0),fill("black"),noStroke(),textSize(15),image(this.img,this.loc.x,this.loc.y,this.w,this.h)}checkMouse(){this.clr=mouseX>this.loc.x&&mouseX<this.loc.x+this.w&&mouseY>this.loc.y&&mouseY<this.loc.y+this.h?"gray":"white"}}'
+           'let page,camLoc,buttL,buttR,timer,buttonImg,pages=[],numOfPages=2,movingRight=!1,movingLeft=!1,isPaused=!1,artWorkPanel="https://tan-hidden-whippet-249.mypinata.cloud/ipfs/"+artWorkIPFS,scoreCardPanel="https://tan-hidden-whippet-249.mypinata.cloud/ipfs/"+scoreCardIPFS,buttonImage="https://tan-hidden-whippet-249.mypinata.cloud/ipfs/"+buttonImageIPFS,defifaBlue=[19,228,240],txt1=[[txt_1,txt_1Color,txt_1Size]],txt2=[[txt_2,txt_2Color,txt_2Size]],txt3=[[txt_3,txt_3Color,txt_3Size]],txt4=[[txt_4,txt_4Color,txt_4Size]],txt5=[[txt_5,txt_5Color,txt_5Size]],txt6=[[txt_6,txt_6Color,txt_6Size]],pageImg=[];function preload(){pageImg[0]=loadImage(artWorkPanel),pageImg[1]=loadImage(scoreCardPanel),buttonImg=loadImage(buttonImage)}function setup(){myFont=loadFont(font),createCanvas(500,500),camLoc=createVector(0,0);for(let a=0;a<numOfPages;a++)pages[a]=new Page(canvas.width/2*a,0,a,pageImg[a]);timer=canvas.width/2,buttL=new Button(canvas.width/2-90,5,75,75,buttonImg),buttR=new Button(canvas.width/2-90,5,75,75,buttonImg)}function draw(){background(220),slide(),push(),translate(camLoc.x,camLoc.y);for(let a=0;a<numOfPages;a++)pages[a].run();pop(),buttL.run(),buttR.run()}function goRight(){movingLeft||isPaused||(movingRight=!0)}function goLeft(){movingRight||isPaused||(movingLeft=!0)}function slide(){movingRight&&!movingLeft&&0<=timer&&(camLoc.x-=20,timer-=20),movingLeft&&!movingRight&&0<=timer&&(camLoc.x+=20,timer-=20),0==timer&&(movingRight=!1,movingLeft=!1,timer=canvas.width/2),0>=-camLoc.x?(camLoc.x=0,buttL.loc.x=-100):buttL.loc.x=canvas.height/2-75,-camLoc.x>=pages[pages.length-1].loc.x?(timer=canvas.width/2,movingRight=!1,movingLeft=!1,camLoc.x=-pages[pages.length-1].loc.x,buttR.loc.x=-100):buttR.loc.x=canvas.height/2-75}function drawtext(a,b,d){for(var e=a,f=0;f<d.length;++f){var g=d[f],h=g[0],j=g[1],c=g[2],k=textWidth(h);fill(j),textSize(c),text(h,e,b),e+=k}}function mousePressed(){mouseX>buttL.loc.x&&mouseX<buttL.loc.x+buttL.w&&mouseY>buttL.loc.y&&mouseY<buttL.loc.y+buttL.h&&goLeft(),mouseX>buttR.loc.x&&mouseX<buttR.loc.x+buttR.w&&mouseY>buttR.loc.y&&mouseY<buttR.loc.y+buttR.h&&goRight()}class Page{constructor(a,b,c,d){this.loc=createVector(a,b),this.w=canvas.width/2,this.h=canvas.height/2,this.pageNum=c+1,this.img=d}run(){image(this.img,this.loc.x,this.loc.y,500,500),textAlign(LEFT),textFont(myFont),2==this.pageNum&&(drawtext(this.loc.x+txt_1_x,this.loc.y+txt_1_y,txt1),drawtext(this.loc.x+txt_2_x,this.loc.y+txt_2_y,txt2),drawtext(this.loc.x+txt_3_x,this.loc.y+txt_3_y,txt3),drawtext(this.loc.x+txt_4_x,this.loc.y+txt_4_y,txt4),drawtext(this.loc.x+txt_5_x,this.loc.y+txt_5_y,txt5),drawtext(this.loc.x+txt_6_x,this.loc.y+txt_6_y,txt6));3==this.pageNum,line(this.loc.x,this.loc.y,this.loc.x+this.w,this.loc.y),line(this.loc.x,this.loc.y,this.loc.x,this.loc.y+this.h),line(this.loc.x,this.loc.y+this.h,this.loc.x+this.w,this.loc.y+this.h),line(this.loc.x+this.w,this.loc.y,this.loc.x+this.w,this.h)}}class Button{constructor(a,b,c,d,e){this.loc=new createVector(a,b),this.w=c,this.h=d,this.clr="white",this.img=e}run(){this.render(),this.checkMouse()}render(){fill(this.clr),stroke(20),strokeWeight(0),fill("black"),noStroke(),textSize(15),image(this.img,this.loc.x,this.loc.y,this.w,this.h)}checkMouse(){this.clr=mouseX>this.loc.x&&mouseX<this.loc.x+this.w&&mouseY>this.loc.y&&mouseY<this.loc.y+this.h?"gray":"white"}}'
         );
  
         requests[2].scriptContent = controllerScript;
         
-        requests[3].name = "defifa_scorecard_min.0.0.1.js";
-        requests[3].wrapType = 1; 
-        requests[3].contractAddress = _ETHFS_FILESTORAGE_ADDRESS;
+        //requests[3].name = 'defifa_scorecard_min.0.0.1.js';
+        //requests[3].contractAddress = _ETHFS_FILESTORAGE_ADDRESS;
+        //requests[3].wrapType = 1; 
         // For easier testing, bufferSize for statically stored scripts 
         // is injected in the constructor. Then controller script's length
         // is added to that to find the final buffer size.
@@ -302,7 +302,13 @@ contract DefifaHTMLTokenUriResolver is IDefifaTokenUriResolver, IJBTokenUriResol
           .getEncodedHTMLWrapped(requests, finalBufferSize);
 
       bytes memory metadata = abi.encodePacked(
-          '{"name":"p5.js Example - GZIP - Base64", "description":"Assembles GZIP compressed base64 encoded p5.js stored in ethfs FileStore contract with a demo scene. Metadata and animation URL are both base64 encoded.","animation_url":"',
+          '{"name":"',
+        _title,
+        '","description":"Team: ',
+        _team,
+        ', ID: ',
+        _tier.id.toString(),
+        '","animation_url":"',
           base64EncodedHTMLDataURI,
           '"}'
       );
